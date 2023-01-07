7f99de7ea97defb1a8d7a77bcb572fd3b7f72d26
Arthur Dayne <118467271+arthur-dane@users.noreply.github.com>
2023-01-07 23:29:14 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1473,22 +1473,36 @@ def unified(url):
 
 def urlsopen(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://short.url2go.in/RJOVAq30CU7lINo9AwG4oT3eISn7"
-    url = url[:-1] if url[-1] == '/' else url
-    code = url.split("/")[-1]
-    final_url = f"{DOMAIN}/{code}"    
-    ref = "https://blog.textpage.xyz/"
-    h = {"referer": ref}
-    resp = client.get(final_url,headers=h)
-    soup = BeautifulSoup(resp.content, "html.parser")
-    inputs = soup.find_all("input")
-    data = { input.get('name'): input.get('value') for input in inputs }
-    h = { "x-requested-with": "XMLHttpRequest" }
-    time.sleep(8)
-    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try: return r.json()['url']
-    except: return "Something went wrong :("
     
+    
+    DOMAIN = "https://blogpost.viewboonposts.com"
+
+    url = url[:-1] if url[-1] == '/' else url
+
+    code = url.split("/")[-1]
+    
+    final_url = f"{DOMAIN}/{code}"
+    
+    ref = "https://blog.textpage.xyz/"
+    
+    h = {"referer": ref}
+  
+    resp = client.get(final_url,headers=h)
+    
+    soup = BeautifulSoup(resp.content, "html.parser")
+    
+    inputs = soup.find_all("input")
+   
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = { "x-requested-with": "XMLHttpRequest" }
+    
+    time.sleep(2)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return r.json()['url']
+    except: return "Something went wrong :("
+   
 
 ####################################################################################################
 # xpshort
@@ -1710,7 +1724,7 @@ def shortners(url):
         return try2link_bypass(url)
 
     # urlsopen
-    elif "https://urlsopen.com/" in url:
+    elif "https://urlsopen.net/" in url:
         print("entered urlsopen:",url)
         return urlsopen(url)
 
