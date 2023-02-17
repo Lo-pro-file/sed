f4720f16d60ef4a53c39ae7a9471a7457d0d0c2e
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-02-17 23:23:17 +0530

    op

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/bipinkrish/Link-Bypasser-Bot /Link-Bypasser-Bot
+  git clone https://github.com/angel-loveyou/sed /sed
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Link-Bypasser-Bot
+  git clone $UPSTREAM_REPO /sed
 fi
-cd /Link-Bypasser-Bot
+cd /sed
 pip3 install -U -r requirements.txt
 echo "Starting Bypass Bot...."
 python3 main.py
